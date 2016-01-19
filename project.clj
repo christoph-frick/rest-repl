916fccb984aafe5349fa6c4cd9601639bd7f1f93
Christoph Frick <rid@ofnir.net>
2016-01-19 23:13:45 +0100

    update to clojure 1.8.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject rest-repl "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.0.0"]
                  [mvxcvi/puget "1.0.0"]
                  [com.rpl/specter "0.9.1"]
