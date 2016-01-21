db65a66697039223045028f482a2724120a24e63
Christoph Frick <rid@ofnir.net>
2016-01-22 00:03:51 +0100

    add command line parsing

--- src/rest/main.clj
+++ src/rest/main.clj
@@ -1,7 +1,36 @@
 (ns rest.main
   (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as s]
+            [clj-http.client :refer [parse-edn]]
+            [rest.public :refer [config]]
             [rest.repl :as repl])
+  (:import [java.io File])
   (:gen-class))
 
+(def ^:const cli-options
+  [["-c" "--config-file config.edn" "Config file to use for @config"
+    :validate [#(.exists (File. %)) "file must exist"]]
+   ["-h" "--help"]])
+
+(defn usage
+  [summary]
+  (str "REST-REPL\n\nrest-repl [options...] [url]\n\n" summary))
+
+(defn error-message
+  [usage errors]
+  (str "ERRORS:\n\n" (s/join "\n" errors) "\n\n" usage))
+
+(defn exit
+  [exit-code message]
+  (println message)
+  (System/exit exit-code))
+
 (defn -main [& args]
-  (repl/run))
+  (let [opts (parse-opts args cli-options)
+        {:keys [options arguments errors summary]} opts]
+    (cond
+      (:help options) (exit 0 (usage summary))
+      errors (exit 1 (error-message (usage summary) errors))
+      (:config-file options) (reset! config (-> options :config-file slurp parse-edn))
+      (seq arguments) (swap! config assoc :base-url (last arguments)))
+    (repl/run)))
