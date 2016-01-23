8ccbf864975edc82b0137c881dc320e61ac83cd9
Christoph Frick <rid@ofnir.net>
2016-01-23 23:14:00 +0100

    add some tests and remove FIMXEs

--- test/rest/core_test.clj
+++ test/rest/core_test.clj
@@ -7,24 +7,25 @@
 (def relcfg {:base-url "http://localhost:8080" :current-url "http://localhost:8080/some"})
 
 (def cd-tests [; abs
-              [abscfg "subdir" "http://localhost:8080/subdir"]
-              [abscfg "subdir/subsubdir" "http://localhost:8080/subdir/subsubdir"]
-              [abscfg "/subdir" "http://localhost:8080/subdir"]
-              [abscfg ".." "http://localhost:8080/"] ; FIXME
-              [abscfg "subdir/../subsubdir" "http://localhost:8080/subsubdir"]
-              [abscfg "subdir/../x1/../x2" "http://localhost:8080/x2"]
-              [abscfg "subdir/../../x1/../../x2" "http://localhost:8080/x2"]
-              [abscfg "~/subdir" "http://localhost:8080/subdir"]
-              [abscfg "http://localhost:5050" "http://localhost:5050"]
+               [abscfg "subdir" "http://localhost:8080/subdir"]
+               [abscfg "subdir/subsubdir" "http://localhost:8080/subdir/subsubdir"]
+               [abscfg "/subdir" "http://localhost:8080/subdir"]
+               [abscfg ".." "http://localhost:8080/"]
+               [abscfg "//////////" "http://localhost:8080/"]
+               [abscfg "subdir/../subsubdir" "http://localhost:8080/subsubdir"]
+               [abscfg "subdir/../x1/../x2" "http://localhost:8080/x2"]
+               [abscfg "subdir/../../x1/../../x2" "http://localhost:8080/x2"]
+               [abscfg "~/subdir" "http://localhost:8080/subdir"]
+               [abscfg "http://localhost:5050" "http://localhost:5050"]
               ; rel
-              [relcfg "subdir" "http://localhost:8080/some/subdir"]
-              [relcfg "subdir/subsubdir" "http://localhost:8080/some/subdir/subsubdir"]
-              [relcfg "/subdir" "http://localhost:8080/some/subdir"]
-              [relcfg "../../subsubdir" "http://localhost:8080/subsubdir"] ; FIXME
-              [relcfg "../subsubdir" "http://localhost:8080/subsubdir"]
-              [relcfg "subdir/../subsubdir" "http://localhost:8080/some/subsubdir"]
-              [relcfg "~/subdir" "http://localhost:8080/subdir"]
-              [relcfg "http://localhost:5050" "http://localhost:5050"]])
+               [relcfg "subdir" "http://localhost:8080/some/subdir"]
+               [relcfg "subdir/subsubdir" "http://localhost:8080/some/subdir/subsubdir"]
+               [relcfg "/subdir" "http://localhost:8080/some/subdir"]
+               [relcfg "../../subsubdir" "http://localhost:8080/subsubdir"]
+               [relcfg "../subsubdir" "http://localhost:8080/subsubdir"]
+               [relcfg "subdir/../subsubdir" "http://localhost:8080/some/subsubdir"]
+               [relcfg "~/subdir" "http://localhost:8080/subdir"]
+               [relcfg "http://localhost:5050" "http://localhost:5050"]])
 
 (deftest chane-dir-tests ; gentest?
   (doseq [[config to result] cd-tests]
@@ -48,4 +49,3 @@
   (is (= (cwd {}) nil))
   (is (= (cwd abscfg) (:base-url abscfg)))
   (is (= (cwd relcfg) (:current-url relcfg))))
-
