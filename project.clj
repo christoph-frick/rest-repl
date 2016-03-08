ea1c735786a2957211f551b0b4c5b6852a23a779
Christoph Frick <rid@ofnir.net>
2016-03-08 11:56:51 +0100

    update to clj-http 2.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rest-repl "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.0.0"]
+                 [clj-http "2.1.0"]
                  [mvxcvi/puget "1.0.0"]
                  [com.rpl/specter "0.9.1"]
                  [cheshire "5.5.0"]
