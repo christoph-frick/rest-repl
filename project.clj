3036980001ee5084aaa0fb1cd8944931d18af7bf
Christoph Frick <rid@ofnir.net>
2016-04-29 22:02:00 +0200

    update versions of clj-http, specter, and cheshire

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject rest-repl "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.1.0"]
+                 [clj-http "3.0.1"]
                  [mvxcvi/puget "1.0.0"]
-                 [com.rpl/specter "0.9.1"]
-                 [cheshire "5.5.0"]
+                 [com.rpl/specter "0.10.0"]
+                 [cheshire "5.6.1"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/tools.cli "0.3.3"]]
   :main rest.main
