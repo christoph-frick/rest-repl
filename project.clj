d7382443bf6a18ae165b9717d7cd2367967719af
Christoph Frick <rid@ofnir.net>
2016-08-31 18:01:44 +0200

    version updates

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject rest-repl "0.2.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.0.1"]
-                 [mvxcvi/puget "1.0.0"]
-                 [com.rpl/specter "0.10.0"]
-                 [cheshire "5.6.1"]
+                 [clj-http "3.2.0"]
+                 [mvxcvi/puget "1.0.1"]
+                 [com.rpl/specter "0.12.0"]
+                 [cheshire "5.6.3"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/tools.cli "0.3.3"]]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main rest.main
   :profiles {:uberjar {:aot :all}})
