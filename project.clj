101343699a662f58adaafadd2676916b407b138d
Christoph Frick <cf@ofnir.net>
2018-02-20 19:55:39 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject rest-repl "0.2.0"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
-                 [clj-http "3.5.0"]
-                 [mvxcvi/puget "1.0.1"]
-                 [com.rpl/specter "1.0.1"]
-                 [cheshire "5.7.1"]
+                 [clj-http "3.7.0"]
+                 [mvxcvi/puget "1.0.2"]
+                 [com.rpl/specter "1.1.0"]
+                 [cheshire "5.8.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/tools.cli "0.3.5"]]
   :main rest.main
