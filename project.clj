1e9cc15ebdb6e66add748982686780093313cffd
Christoph Frick <cf@ofnir.net>
2018-03-12 12:17:05 +0100

    Update clj-http

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject rest-repl "0.2.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
-                 [clj-http "3.7.0"]
+                 [clj-http "3.8.0"]
                  [mvxcvi/puget "1.0.2"]
                  [com.rpl/specter "1.1.0"]
                  [cheshire "5.8.0"]
