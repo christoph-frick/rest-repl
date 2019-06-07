a129623cfcbe4f82324dc05ce7a8a534021d2c5c
Christoph Frick <cf@ofnir.net>
2019-06-07 07:24:05 +0200

    Update to Clojure 1.10.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject net.ofnir/rest-repl "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [net.ofnir/wrepl "0.1.0-SNAPSHOT"]
                  [net.ofnir/wrepl.puget "0.1.0-SNAPSHOT"]
                  [net.ofnir/wrepl.pomegranate "0.1.0-SNAPSHOT"] 
